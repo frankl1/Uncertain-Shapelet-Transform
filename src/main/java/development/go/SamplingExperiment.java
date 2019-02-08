f151b827c3ef1b569cc4eeb12595775fa14dcdc4
George Oastler <goastler4@gmail.com>
2019-02-08 12:34:50 +0000

    Fixed killswitch and resampling

--- src/main/java/development/go/SamplingExperiment.java
+++ src/main/java/development/go/SamplingExperiment.java
@@ -11,14 +11,12 @@ import utilities.ClassifierResults;
 import utilities.ClassifierStats;
 import utilities.InstanceTools;
 import utilities.Utilities;
-import utilities.instances.Folds;
 import utilities.range.Range;
 import weka.core.Instances;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.zip.GZIPOutputStream;
 
@@ -27,8 +25,8 @@ public class SamplingExperiment {
     private SamplingExperiment() {}
 
     // todo param validation
-    @Parameter(names={"-r"}, description="results dir", converter= FileConverter.class, required=true)
-    private File dir;
+    @Parameter(names={"-r"}, description="results globalResultsDir", converter= FileConverter.class, required=true)
+    private File globalResultsDir;
     @Parameter(names={"-fi"}, description="dataset fold index", required=true)
     private List<Integer> foldIndices;
     @Parameter(names={"-d"}, description="datasets", required=true)
@@ -63,8 +61,8 @@ public class SamplingExperiment {
         out.close();
     }
 
-    private static void writeStatsToFile(Instances testInstances, NearestNeighbour nearestNeighbour, File resultsDir, String resultsFilePrefix, double index, long benchmark) throws IOException {
-        File file = new File(resultsDir, resultsFilePrefix + ",p=" + index + ".gzip");
+    private boolean writeStatsToFile(String resultsFilePrefix, double percentage) throws IOException {
+        File file = new File(resultsDir, resultsFilePrefix + ",p=" + percentage + ".gzip");
         if(file.createNewFile()) {
             double[][] predictions = nearestNeighbour.predict();
             ClassifierResults results = new ClassifierResults();
@@ -79,8 +77,15 @@ public class SamplingExperiment {
             results.setBenchmark(benchmark);
             ClassifierStats stats = new ClassifierStats(results);
             writeObjectToFile(stats, file);
+            return true;
         }
+        return false;
     }
+
+    private long benchmark;
+    private File resultsDir;
+    private NearestNeighbour nearestNeighbour;
+    private Instances testInstances;
 
     public void run() {
         final boolean[] stop = {false};
@@ -99,17 +104,17 @@ public class SamplingExperiment {
             }
         });
         killSwitch.start();
-        long benchmark = ClassifierResults.benchmark();
+        benchmark = ClassifierResults.benchmark();
         List<ParameterisedSupplier<? extends DistanceMeasure>> parameterisedSuppliers = new ArrayList<>();
-        parameterisedSuppliers.add(new DtwParameterisedSupplier());
-        parameterisedSuppliers.add(new DdtwParameterisedSupplier());
-        parameterisedSuppliers.add(new WdtwParameterisedSupplier());
-        parameterisedSuppliers.add(new WddtwParameterisedSupplier());
+//        parameterisedSuppliers.add(new DtwParameterisedSupplier());
+//        parameterisedSuppliers.add(new DdtwParameterisedSupplier());
+//        parameterisedSuppliers.add(new WdtwParameterisedSupplier());
+//        parameterisedSuppliers.add(new WddtwParameterisedSupplier());
         parameterisedSuppliers.add(new LcssParameterisedSupplier());
-        parameterisedSuppliers.add(new MsmParameterisedSupplier());
-        parameterisedSuppliers.add(new TweParameterisedSupplier());
-        parameterisedSuppliers.add(new ErpParameterisedSupplier());
-        parameterisedSuppliers.add(new EuclideanParameterisedSupplier());
+//        parameterisedSuppliers.add(new MsmParameterisedSupplier());
+//        parameterisedSuppliers.add(new TweParameterisedSupplier());
+//        parameterisedSuppliers.add(new ErpParameterisedSupplier());
+//        parameterisedSuppliers.add(new EuclideanParameterisedSupplier());
         final int[] parameterBins = new int[] {
             parameterisedSuppliers.size(),
             datasets.size(),
@@ -118,6 +123,7 @@ public class SamplingExperiment {
         for(Integer foldIndex : foldIndices) {
             RandomIndexIterator combinationIndexIterator = new RandomIndexIterator();
             combinationIndexIterator.setRange(new Range(0, numCombinations - 1));
+            combinationIndexIterator.setSeed(0);
             while (combinationIndexIterator.hasNext() && !stop[0]) {
                 int combination = combinationIndexIterator.next();
                 combinationIndexIterator.remove();
@@ -126,43 +132,49 @@ public class SamplingExperiment {
                 ParameterisedSupplier<? extends DistanceMeasure> parameterisedSupplier = parameterisedSuppliers.get(parameters[parameterIndex++]);
                 File datasetFile = datasets.get(parameters[parameterIndex++]);
                 String datasetName = datasetFile.getName();
-                File experimentResultDir = new File(dir, datasetName);
-                mkdir(experimentResultDir);
+                resultsDir = new File(globalResultsDir, datasetName);
+                mkdir(resultsDir);
                 try {
                     Instances dataset = Utilities.loadDataset(datasetFile);
                     Instances[] splitInstances = InstanceTools.resampleInstances(dataset, foldIndex, 0.5);
                     Instances trainInstances = splitInstances[0];
                     parameterisedSupplier.setParameterRanges(trainInstances);
-                    Instances testInstances = splitInstances[1];
+                    testInstances = splitInstances[1];
                     RandomIndexIterator distanceMeasureParameterIterator = new RandomIndexIterator();
                     distanceMeasureParameterIterator.setRange(new Range(0, parameterisedSupplier.size() - 1));
+                    distanceMeasureParameterIterator.setSeed(0);
                     while(distanceMeasureParameterIterator.hasNext() && !stop[0]) {
                         int distanceMeasureParameter = distanceMeasureParameterIterator.next();
                         distanceMeasureParameterIterator.remove();
                         DistanceMeasure distanceMeasure = parameterisedSupplier.get(distanceMeasureParameter);
-                        NearestNeighbour nearestNeighbour = new NearestNeighbour();
+                        nearestNeighbour = new NearestNeighbour();
                         nearestNeighbour.setDistanceMeasure(distanceMeasure);
-                        // check whether results exist
                         String resultsFilePrefix = "f=" + foldIndex
                             + ",k=" + nearestNeighbour.getK()
                             + ",d=" + nearestNeighbour.getDistanceMeasure()
                             + ",q={" + nearestNeighbour.getDistanceMeasure().getParameters() + "}";
+//                        System.out.println(resultsFilePrefix);
                         nearestNeighbour.setSeed(foldIndex);
                         nearestNeighbour.setTrainInstances(trainInstances);
                         nearestNeighbour.setTestInstances(testInstances);
                         nearestNeighbour.train();
                         int index = 0;
+                        double percentage = 0;
                         double nextPercentage = 0;
                         double percentageIncrement = 0.01;
-                        if(!stop[0]) writeStatsToFile(testInstances, nearestNeighbour, experimentResultDir, resultsFilePrefix, index++, benchmark);
+                        boolean noExistingResults = true;
+                        if(!stop[0]) {
+                            noExistingResults = writeStatsToFile(resultsFilePrefix, percentage);
+                        }
+                        index++;
                         nextPercentage += percentageIncrement;
-                        while (nearestNeighbour.remainingTestTicks() && !stop[0]) {
+                        while (nearestNeighbour.remainingTestTicks() && !stop[0] && noExistingResults) {
                             nearestNeighbour.testTick();
                             if(nearestNeighbour.hasSelectedNewTrainInstance()) {
-                                double percentage = (double) index / trainInstances.numInstances();
+                                percentage = (double) index / trainInstances.numInstances();
                                 if(percentage >= nextPercentage) {
                                     try {
-                                        writeStatsToFile(testInstances, nearestNeighbour, experimentResultDir, resultsFilePrefix, percentage, benchmark);
+                                        noExistingResults = writeStatsToFile(resultsFilePrefix, percentage);
                                     } catch (Exception e) {
                                         e.printStackTrace();
                                     }
@@ -171,13 +183,16 @@ public class SamplingExperiment {
                                 index++;
                             }
                         }
-                        if(!stop[0]) {
+                        if(!stop[0] && noExistingResults) {
                             try {
-                                writeStatsToFile(testInstances, nearestNeighbour, experimentResultDir, resultsFilePrefix, 1, benchmark);
+                                noExistingResults = writeStatsToFile(resultsFilePrefix, 1);
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
                         }
+//                        if(!noExistingResults) {
+//                            System.out.println("skip");
+//                        }
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
