c2443d2b13cedb17eaf01943f6578595de487397
George Oastler <goastler4@gmail.com>
2019-03-27 10:19:20 +0000

    Fixed distance weighting

--- src/main/java/timeseriesweka/classifiers/nn/NeighbourWeighting/WeightByDistance.java
+++ src/main/java/timeseriesweka/classifiers/nn/NeighbourWeighting/WeightByDistance.java
@@ -3,6 +3,6 @@ package timeseriesweka.classifiers.nn.NeighbourWeighting;
 public class WeightByDistance implements NeighbourWeighter {
     @Override
     public double weight(final double distance) {
-        return distance;
+        return 1 / (1 + distance);
     }
 }
