de59da83cb3fb9e95f83cc38099acbb73eb5e0a5
George Oastler <goastler4@gmail.com>
2019-03-27 17:19:32 +0000

    cleanup

--- src/main/java/timeseriesweka/classifiers/AdvancedClassifier.java
+++
@@ -1,9 +0,0 @@
-package timeseriesweka.classifiers;
-
-import utilities.ClassifierResults;
-import utilities.Reproducible;
-import utilities.SaveParameterInfo;
-import weka.core.Instances;
-
-public interface AdvancedClassifier extends weka.classifiers.Classifier, Reproducible, ContractClassifier, CheckpointClassifier, SaveParameterInfo {
-}
